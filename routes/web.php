1855db139bade118fb557479b537f99792901297
tomori <tomori.study@gmail.com>
2023-01-19 08:39:38 +0000

    post

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,8 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UsersController; // 追記
+use App\Http\Controllers\UsersController;      // 追記
+use App\Http\Controllers\MicropostsController; // 追記
 
 /*
 |--------------------------------------------------------------------------
@@ -15,13 +16,17 @@ use App\Http\Controllers\UsersController; // 追記
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+// 
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/', [MicropostsController::class, 'index']);
+
+Route::get('/dashboard', [MicropostsController::class, 'index'])->middleware(['auth'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -31,6 +36,7 @@ Route::middleware('auth')->group(function () {
 
 require __DIR__.'/auth.php';
 
-Route::group(['middleware' => ['auth']], function () {                                    // 追記
-    Route::resource('users', UsersController::class, ['only' => ['index', 'show']]);     // 追記
-});                                                                                       // 追記
+Route::group(['middleware' => ['auth']], function () {                                            // 追記
+    Route::resource('users', UsersController::class, ['only' => ['index', 'show']]);              // 追記
+    Route::resource('microposts', MicropostsController::class, ['only' => ['store', 'destroy']]); // 追記
+});                                                                                               // 追記
