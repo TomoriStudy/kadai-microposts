cd29c9ab832a4c360c6397d64eee7d5948a88901
tomori <tomori.study@gmail.com>
2023-01-19 08:06:32 +0000

    user pages

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UsersController; // 追記
 
 /*
 |--------------------------------------------------------------------------
@@ -15,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('dashboard');
+    return view('welcome');
 });
 
 Route::get('/dashboard', function () {
@@ -29,3 +30,7 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::group(['middleware' => ['auth']], function () {                                    // 追記
+    Route::resource('users', UsersController::class, ['only' => ['index', 'show']]);     // 追記
+});                                                                                       // 追記
